708eb0969038e51d3b3c80343e5e1e04436c97e8
wendelcliques <70993835+wendelcliques@users.noreply.github.com>
2021-04-02 23:45:09 -0300

    atual

--- tccCalculoEmCliques/App.js
+++ tccCalculoEmCliques/App.js
@@ -70,7 +70,7 @@ const App: () => Node = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One 123">
+          <Section title="Step One 12">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
           </Section>
--- tccCalculoEmCliques/ios/CálculoemCliquesTests/CálculoemCliquesTests.m
+++ tccCalculoEmCliques/ios/CálculoemCliquesTests/CálculoemCliquesTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface tccCalculoEmCliquesTests : XCTestCase
+@interface CálculoemCliquesTests : XCTestCase
 
 @end
 
-@implementation tccCalculoEmCliquesTests
+@implementation CálculoemCliquesTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
